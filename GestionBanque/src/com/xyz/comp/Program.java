98583ebd3eff302c147025ffe8d4eef3c1c813ac
Rhazouani imadeddine <146865915+ImadRhaz@users.noreply.github.com>
2024-03-19 23:03:37 +0100

    Update Program.java

--- GestionBanque/src/com/xyz/comp/Program.java
+++ GestionBanque/src/com/xyz/comp/Program.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class Program {
 
     public static void main(String[] args) throws ParseException {
-        Client client = new Client(1, "IMAD", "RHAZOUANI");
-        Compte compte = new Compte(1, "A1234", client);
-        client.setCompte(compte);
+        Client client = new Client(1, "IMAD", "RHAZOUANI"); // Creer d'abord Client
+        Compte compte = new Compte(1, "A1234", client);// Puis Creer Compte
+        client.setCompte(compte); // Associer le compte au client
 
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
-        Operation op1 = new Operation(sdf.parse("2024-03-18"), "VERS", 4000.0,compte);
-        compte.addOperation(op1);
+        Operation op1 = new Operation(sdf.parse("2024-03-18"), "VERS", 4000.0,compte);//Creer L'operation voulu
+        compte.addOperation(op1); // Associer l'operation au compte 
 
         Operation op2 = new Operation(sdf.parse("2024-03-18"), "VERS", 5000.0,compte);
         compte.addOperation(op2);
@@ -25,10 +25,10 @@ public class Program {
         Operation op4 = new Operation(sdf.parse("2024-03-19"), "VERS", 5000.0,compte);
         compte.addOperation(op4);
         System.out.println("Test");
-        afficher(compte);
+        afficher(compte); // Faire Appel a la Methode afficher 
     }
 
-    public static void afficher(Compte compte) {
+    public static void afficher(Compte compte) { // Creer une methode statique => Afin de l'appeler sans creer un Objet de la class Program
         SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
 
         System.out.println("Client " + compte.getClient().getNom() + " " + compte.getClient().getPrenom());
